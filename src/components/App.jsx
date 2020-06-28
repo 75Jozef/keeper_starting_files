fc952706bcfbf7980b62bcc8bdc62941eaeb12a3
75Jozef <64250704+75Jozef@users.noreply.github.com>
2020-06-28 22:26:35 +0200

    nefunguje

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, updateNotes] = useState({
+    key: 0,
+    id: 0,
+    title: "",
+    note: ""
+  });
+
+  function addNote() {
+    const newNote = { title: titleText, note: noteText };
+    updateNotes(prevNotes => {
+      return { ...prevNotes, newNote };
+    });
+  }
+
+  function deleteItem(id) {
+    updateNotes(prevNotes => {
+      return prevNotes.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onClick={addNote} />
+
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onChecked={deleteItem}
+          />
+        );
+      })}
+
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+  const [titleText, changeTitleText] = useState("");
+  const [noteText, changeNoteText] = useState("");
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    if (name === "title") {
+      changeTitleText(value);
+    } else {
+      changeNoteText(value);
+    }
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+          value={titleText}
+          autoComplete="off"
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          autoComplete="off"
+          value={noteText}
+        />
+        <button onClick={addNote}>Add</button>
       </form>
     </div>
   );
